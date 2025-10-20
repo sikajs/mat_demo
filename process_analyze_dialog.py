8c0035fb90f6c2b487d31caf5691d1baebf8fe27
Jason Shen <sikajs@gmail.com>
2025-10-20 23:29:38 +0800

    feature/add_country_of_origin_percentage_in_process_material_analysis

--- process_analyze_dialog.py
+++ process_analyze_dialog.py
@@ -1,5 +1,6 @@
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QDialog, QPushButton, QGridLayout, QLabel, QComboBox, QTableWidget, QTableWidgetItem
+import pandas as pd
 
 from add_material_dialog import AddMaterialDialog
 
@@ -7,33 +8,37 @@ class ProcessAnalyzeDialog(QDialog):
     def __init__(self, main_window):
         super().__init__()
         self.setWindowTitle("Usage Process Analysis")
-        self.setFixedWidth(600)
+        self.setFixedWidth(800)
         self.setFixedHeight(600)
 
         self.main_window = main_window
         self.selected_materials = []
         self.total_cost = None
 
-        layout = QGridLayout()
+        self.layout = QGridLayout()
 
         self.process_combo = QComboBox()
         self.process_combo.addItems(['- Select process -'] + self.process_list)
 
         self.table = QTableWidget()
-        self.table.setColumnCount(5)
-        self.table.setHorizontalHeaderLabels(['Material id', 'Material Name', 'Unit', 'Unit count', 'Total Cost (USD)'])
-
-        self.total_cost_label = QLabel("Total Cost (USD): 0")
+        self.table.setColumnCount(6)
+        self.table.setHorizontalHeaderLabels(['Material id', 'Material Name', 'Unit', 'Unit count', 'Total Cost (USD)', 'Country of origin'])
 
         button = QPushButton("Add material")
         button.clicked.connect(self.add_material)
 
-        layout.addWidget(self.process_combo, 0, 0)
-        layout.addWidget(button, 1, 0)
-        layout.addWidget(self.table, 2, 0, 1, 2)
-        layout.addWidget(self.total_cost_label, 3, 1)
+        self.total_cost_label = QLabel("Total Cost (USD): 0")
+        self.country_table = QTableWidget()
+        self.country_table.setColumnCount(2)
+        self.country_table.setHorizontalHeaderLabels(['Country of origin', 'Cost percentage'])
 
-        self.setLayout(layout)
+        self.layout.addWidget(self.process_combo, 0, 0)
+        self.layout.addWidget(button, 1, 0)
+        self.layout.addWidget(self.table, 2, 0, 1, 2)
+        self.layout.addWidget(self.total_cost_label, 3, 1)
+        self.layout.addWidget(self.country_table, 4, 0)
+
+        self.setLayout(self.layout)
 
     def add_material(self):
         if self.process_combo.currentText() != '- Select process -':
@@ -49,10 +54,21 @@ class ProcessAnalyzeDialog(QDialog):
             material_ids = [item['material_id'] for item in self.selected_materials]
             quote_ids = [f"'{mid}'" for mid in material_ids]
             material_ids_str = ','.join(quote_ids)
-            sql = f"SELECT material_id, material_name, unit, unit_cost_usd FROM materials WHERE material_id in ({material_ids_str})"
+            sql = f"SELECT material_id, material_name, unit, unit_cost_usd, country_of_origin FROM materials WHERE material_id in ({material_ids_str})"
             result = self.main_window.db.query(sql)
             self.form_materials_list(result, materials_list)
         self.refresh_table(materials_list)
+        self.calculate_courtry_percentage(materials_list)
+
+    def calculate_courtry_percentage(self, materials_list):
+        df = pd.DataFrame(materials_list, columns=['material_id', 'name', 'unit', 'unit_count', 'item_cost', 'country_of_origin'])
+        self.country_table.setRowCount(0)
+        for country, group in df.groupby('country_of_origin'):
+            percentage = round((group['item_cost'].sum() / self.total_cost * 100), 2)
+            row = self.country_table.rowCount()
+            self.country_table.insertRow(row)
+            self.country_table.setItem(row, 0, QTableWidgetItem(str(country)))
+            self.country_table.setItem(row, 1, QTableWidgetItem(f"{percentage}%"))
 
     def find_count(self, material_id):
         result = next((item for item in self.selected_materials if item['material_id'] == material_id), None)
@@ -63,15 +79,22 @@ class ProcessAnalyzeDialog(QDialog):
             unit_count = float(self.find_count(item[0]))
             item_cost = round(float(item[3]) * unit_count, 2)
             self.total_cost += item_cost
-            materials_list.append((item[0], item[1], item[2], unit_count, item_cost))
+            materials_list.append({
+                'material_id': item[0],
+                'name': item[1],
+                'unit': item[2],
+                'unit_count': unit_count,
+                'item_cost': item_cost,
+                'country_of_origin': item[4],
+            })
 
     def refresh_table(self, materials):
         if len(materials) > 0:
             self.table.setRowCount(0)
             for row_num, row_data in enumerate(materials):
                 self.table.insertRow(row_num)
-                for col_num, col_data in enumerate(row_data):
-                    self.table.setItem(row_num, col_num, QTableWidgetItem(str(col_data)))
+                for col_num, (key, value) in enumerate(row_data.items()):
+                    self.table.setItem(row_num, col_num, QTableWidgetItem(str(value)))
         self.total_cost_label.setText("Total Cost (USD): " + str(round(self.total_cost, 2)))
 
     @property
